99fe847e1fc00411adc53d0b5d57ea0f91271986
lilin <118693994+lilinlim@users.noreply.github.com>
2023-09-09 18:29:04 -0400

    editing shapes

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -20,6 +20,16 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(500, 500);
+
+    background(255, 192, 203);
+
+    ellipseMode(CORNER);
+    ellipse(250, 250, 100, 100);
+    ellipse(250, 250, 80, 80);
+    ellipse(250, 250, 60, 60);
+    ellipse(250, 250, 40, 40);
+    ellipse(250, 250, 20, 20);
 
 }
 
