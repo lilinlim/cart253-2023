b5a9f4b261722d28a18a052ce9b93e6d7c74b587
lilin <118693994+lilinlim@users.noreply.github.com>
2023-09-21 00:09:13 -0400

    finish tut 4.1

--- experiments/conditionals-experiments/js/script.js
+++ experiments/conditionals-experiments/js/script.js
@@ -6,27 +6,40 @@
  * and this description to match your project!
  */
 
-"use strict";
-
-/**
- * Description of preload
-*/
-function preload() {
-
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 5
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(backgroundShade);
+
+    circle.x = circle.x + circle.speed;
+
+    if(circle.x > width) {
+        circle.speed = -circle.speed;
+    }
+
+    if(circle.x < 0) {
+        circle.speed = -circle.speed;
+    }
+
+    if(mouseY < heigth/2){
+        fill(255, 0, 0);
+    }
+
+    if(mouseY > heigth/2){
+        fill(0, 0, 255);
+    }
+
+    ellipse(circle.x, circle.y, circle.size);
 
 }
