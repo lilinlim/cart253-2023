871c80494cdb79afd644640000b83070d23fb325
lilin <118693994+lilinlim@users.noreply.github.com>
2023-10-21 16:38:15 -0400

    switch back to mousepressed + adding sim

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -1,3 +1,14 @@
+let rice = {
+    x: 200,
+    y: 200,
+    size: 200,
+    fill: {
+      r: 255,
+      g: 223,
+      b: 216
+  }
+}
+  
 let state = 'title';
 
 function preload(){ 
@@ -16,65 +27,70 @@ function draw() {
     background(220);
   
     if(state === 'title'){
-        title();
-  }
-  else if (state === 'simulation'){
+        title()
+      }
+      else if(state === 'explanation'){
+        explanation();
+      }
+      else if(state === 'simulation'){
         simulation();
-  }
-  else if (state === 'end'){
+      }
+      else if(state === 'end'){
         ending();
-  }
-  else if (state === 'testo'){
-        test();
-  }
-  else if (state === 'eggo'){
-        eg();
-  }
-  else if (state === 'exa2'){
-        ex2();
-  }
-  else if (state === 'exa3'){
-        ex2();
-  }
+      }
+      else if(state === 'credits'){
+        credits();
+      }
 }
-
-function mouseClicked(){
-    if(state === 'title'){
-        state = 'simulation';
-   }
-    else if(state === 'simulation'){
-        state = 'end';
-    }
-  else if (state === 'end'){
-           state = 'testo';
-    }
-  else if(state === 'testo'){
-          state = 'eggo';
-          }
-  
-  else if (state === 'eggo'){
-           state = 'exa2';
-    }
-}
-
+    
 function title(){
     push();
     textSize(64);
     fill(200, 100, 100);
     textAlign(CENTER, CENTER);
-    text('click to start', width/2, height/1.25);
+    text('title', width/2, height/2);
     pop();
-}
-
-function simulation(){
+ }
+    
+function explanation(){
     push();
     textSize(64);
     fill(200, 100, 100);
     textAlign(CENTER, CENTER);
-    text('nyaaa', width/2, height/1.25);
+    text('blablabla', width/2, height/1.25);
     pop();
 }
-
+    
+function simulation(){
+      
+    noStroke();
+    fill(rice.fill.r, rice.fill.g, rice.fill.b);
+    ellipse(rice.x, rice.y, rice.size);
+      
+    //if(rice.size === 170){rice.fill(255, 204, 230);}
+      
+}
+    
+function mousePressed(){
+      
+    if(state === 'title'){
+        state = 'explanation';
+    }
+    else if(state === 'explanation'){
+        state = 'simulation'
+    }
+    else if(state === 'simulation'){
+      rice.size = rice.size - 10;
+      if(rice.size === 150){
+        state = 'end';
+      }
+    }
+    else if(state === 'end'){
+        state = 'credits';
+    }
+      
+}
+    
 function ending(){
     push();
     textSize(64);
@@ -83,39 +99,12 @@ function ending(){
     text('end??', width/2, height/1.25);
     pop();
 }
-
-function test(){
+    
+function credits(){
     push();
     textSize(64);
     fill(200, 100, 100);
     textAlign(CENTER, CENTER);
-    text('test123', width/2, height/1.25);
-    pop();
-}
-
-function eg(){
-    push();
-    textSize(64);
-    fill(200, 100, 100);
-    textAlign(CENTER, CENTER);
-    text('egg.', width/2, height/1.25);
-    pop();
-}
-
-function ex2(){
-    push();
-    textSize(64);
-    fill(200, 100, 100);
-    textAlign(CENTER, CENTER);
-    text('ex2.', width/2, height/1.25);
-    pop();
-}
-
-function ex3(){
-    push();
-    textSize(64);
-    fill(200, 100, 100);
-    textAlign(CENTER, CENTER);
-    text('ex3.', width/2, height/1.25);
+    text('credits', width/2, height/1.25);
     pop();
 }
